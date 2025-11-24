ebcdd9c5684f4d47e1513f6a2f592d82ba457c90
FatemehOstvar <fatemehostvar2005@gmail.com>
2025-11-24 10:17:02 +0330

    added routers do server

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const express= require('express')
 const app = express()
 require('dotenv').config()
+const passport = require('passport')
+require('./config/passport')(passport);
+app.use(passport.initialize())
+app.use(passport.session()); //app.use(passport.authenticate('session'));
 
 const port = process.env.PORT || 3010
-const {memberRouter}= require('./routers/members')
 
+const {spectatorRouter} = require('./routers/spectators');
+const {adminRouter} = require('./routers/admin');
+const {userRouter} = require('./routers/members');
+
+const {join} = require("node:path");
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
 
+const assetsPath = join(__dirname, "public");
+app.use(express.static('public'))
+app.use(express.static(assetsPath));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-app.use('/',memberRouter)
-
+app.use((req,res,next)=>{
+    res.locals.err = null;
+    res.locals.messages = null;
+    res.locals.roleName = null;
+    next()
+})
+app.use('/',spectatorRouter)
+app.use('/admin', adminRouter)
+app.use('/user', userRouter)
 app.listen(port, () => {
     console.log(`Server listening on port http://localhost:${port}`)
 })
+
+
